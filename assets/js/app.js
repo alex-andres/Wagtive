c2a48610e627f7d2aa46eca39e40a8c28f8698de
c-andres <charlesalexanderandres@gmail.com>
2019-01-17 11:30:29 -0800

    added new db to wagtive app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,30 @@
-    // Initialize Firebase
+// Initialize Firebase
 
-    var config = {
-        apiKey: "AIzaSyBH-OgVkuMyOaep8meq0FJD1RxavuIlXlY",
-        authDomain: "wagtive.firebaseapp.com",
-        databaseURL: "https://wagtive.firebaseio.com",
-        projectId: "wagtive",
-        storageBucket: "wagtive.appspot.com",
-        messagingSenderId: "248580578313"
-    };
-    firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyCw7hB7UoQQRk_-ZVm3uyxB0ybPz5WIKaM",
+    authDomain: "wagtive-ec4ee.firebaseapp.com",
+    databaseURL: "https://wagtive-ec4ee.firebaseio.com",
+    projectId: "wagtive-ec4ee",
+    storageBucket: "wagtive-ec4ee.appspot.com",
+    messagingSenderId: "1082781691019"
+};
+firebase.initializeApp(config);
 
-    const db = firebase.database();
-    const auth = firebase.auth();
+const db = firebase.database();
+const auth = firebase.auth();
 
-    var firstName;
-    var lastName;
-    var email;
-    var password;
-    var hTow;
-    var petName;
+var firstName;
+var lastName;
+var email;
+var password;
+var hTow;
+var petName;
 
-    var user;
+var user;
 
-    $("#infoTooltip").hover(function () {
-        $('[data-toggle="tooltip"]').tooltip("show")
-    },
-    function () {
-        $('[data-toggle="tooltip"]').tooltip("hide")
-    })
+$("#infoTooltip").hover(function () {
+    $('[data-toggle="tooltip"]').tooltip("show")
+},
+function () {
+    $('[data-toggle="tooltip"]').tooltip("hide")
+})
